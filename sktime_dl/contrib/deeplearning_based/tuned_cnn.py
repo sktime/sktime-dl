ad1742b116785b527ae9cf35380c00831403b964
mloning <markus.loning.17@ucl.ac.uk>
2019-07-26 14:42:54 +0100

    removed test from file

--- sktime_dl/contrib/deeplearning_based/tuned_cnn.py
+++ sktime_dl/contrib/deeplearning_based/tuned_cnn.py
@@ -22,7 +22,6 @@ __author__ = "James Large"
 import numpy as np
 
 from sktime_dl.contrib.deeplearning_based.basenetwork import BaseDeepLearner
-from sktime_dl.classifiers.tests.test_dl4tscnetworks import test_network
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
@@ -108,9 +107,3 @@ class Tuned_CNN(BaseDeepLearner):
         params = self.grid_history.cv_results_['params']
         for mean, stdev, param in zip(means, stds, params):
             print("%f (%f) with: %r" % (mean, stdev, param))
-
-
-if __name__ == '__main__':
-    # simple, small, fast search for testing. default nb_epochs = 2000
-    param_grid = dict(nb_epochs=[5, 10])
-    test_network(Tuned_CNN(param_grid=param_grid, cv_folds=2), verbose=True)
