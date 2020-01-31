124312ab1fcd17278bfc9bf044d51620e60a2221
Lucy <lucy.withington@outlook.com>
2020-01-31 09:10:14 +0000

    Manually merged in the save_trained_model function from dev branch.

--- sktime_dl/meta/tests/test_ensembling.py
+++ sktime_dl/meta/tests/test_ensembling.py
@@ -25,7 +25,7 @@ def test_basic_inmem(network=DeepLearnerEnsembleClassifier(
     X_train, y_train = load_italy_power_demand(split='TRAIN', return_X_y=True)
     X_test, y_test = load_italy_power_demand(split='TEST', return_X_y=True)
 
-    hist = network.fit(X_train[:10], y_train[:10])
+    __ = network.fit(X_train[:10], y_train[:10])
 
     print(network.score(X_test[:10], y_test[:10]))
     print("End test_basic()")
