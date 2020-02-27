d801f578d55cc1aa0701f6df5bf4df4747c06ae1
James Large <james.large@uea.ac.uk>
2020-02-27 18:06:12 +0000

    missed var name changes

--- sktime_dl/deeplearning/tlenet/_classifier.py
+++ sktime_dl/deeplearning/tlenet/_classifier.py
@@ -100,7 +100,7 @@ class TLENETClassifier(BaseDeepClassifier, TLENETNetwork):
         # ignore the number of instances, X.shape[0], just want the shape of each instance
         self.input_shape = X.shape[1:]
 
-        self.nb_classes = y.shape[1]
+        self.nb_classes = y_onehot.shape[1]
 
         self.adjust_parameters(X)
         X, y_onehot, tot_increase_num = self.pre_processing(X, y_onehot)
@@ -108,7 +108,7 @@ class TLENETClassifier(BaseDeepClassifier, TLENETNetwork):
         input_shape = X.shape[1:]  # pylint: disable=E1136  # pylint/issues/3139
         self.model = self.build_model(input_shape, self.nb_classes)
 
-        self.hist = self.model.fit(X, y, batch_size=self.batch_size, epochs=self.nb_epochs,
+        self.hist = self.model.fit(X, y_onehot, batch_size=self.batch_size, epochs=self.nb_epochs,
                                    verbose=self.verbose, callbacks=self.callbacks)
 
         self.save_trained_model()
