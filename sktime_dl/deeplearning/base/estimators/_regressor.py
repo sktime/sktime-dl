2112510dc0f3b432221cc9f2d2a56d3a0b986111
James Large <james.large@uea.ac.uk>
2020-02-28 15:39:06 +0000

    cleanup

--- sktime_dl/deeplearning/base/estimators/_regressor.py
+++ sktime_dl/deeplearning/base/estimators/_regressor.py
@@ -59,35 +59,6 @@ class BaseDeepRegressor(BaseRegressor, RegressorMixin):
             y_pred.ravel()
         return y_pred
 
-    # def check_and_clean_data(self, X, y=None, input_checks=True):
-    #     if input_checks:
-    #         if y is None:
-    #             validate_X(X)
-    #         else:
-    #             validate_X_y(X, y)
-    #
-    #     # want data in form: [instances = n][timepoints = m][dimensions = d]
-    #     if isinstance(X, pd.DataFrame):
-    #         if X.shape[1] > 1:
-    #             # we have multiple columns
-    #             # if cells contain series, this is a multidimensional problem
-    #             # else if cells contain single values, this is a univariate problem with values long columns
-    #             #      this situation can happen with e.g. forecasting-reduced-to-regression strategies
-    #             if isinstance(X.iloc[0, 0], pd.Series):
-    #                 # todo investigate incorporating the reshaping into the data extraction instead of this 2-stage process
-    #                 X = np.array([[X.iloc[r, c].values for c in range(len(X.columns))] for r in range(len(X))])
-    #                 X = X.reshape(X.shape[0], X.shape[2], X.shape[1])  # go from [n][d][m] to [n][m][d]
-    #             else:
-    #                 X = X.to_numpy()
-    #         else:
-    #             X = np.asarray([a.values for a in X.iloc[:, 0]])
-    #
-    #     if len(X.shape) == 2:
-    #         # add a dimension to make it multivariate with one dimension
-    #         X = X.reshape(X.shape[0], X.shape[1], 1)  # go from [n][m] to [n][m][d=1]
-    #
-    #     return X
-
     def save_trained_model(self):
         save_trained_model(
             self.model,
--- sktime_dl/utils/_data.py
+++ sktime_dl/utils/_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+from sktime.utils.data_container import tabularise
+
 from sktime.utils.validation.supervised import validate_X, validate_X_y
 
 def check_and_clean_data(X, y=None, input_checks=True):
@@ -10,30 +12,31 @@ def check_and_clean_data(X, y=None, input_checks=True):
         else:
             validate_X_y(X, y)
 
-    from sktime.utils.data_container import tabularise
-
-    print(X.shape, X.iloc[0 ,0].shape)
-    X = tabularise(X, return_array=True)
-    print(X.shape)
-
     # want data in form: [instances = n][timepoints = m][dimensions = d]
     if isinstance(X, pd.DataFrame):
-        if X.shape[1] > 1:
-            # we have multiple columns
-            # if cells contain series, this is a multidimensional problem
-            # else if cells contain single values, this is a univariate problem with values long columns
-            #      this situation can happen with e.g. forecasting-reduced-to-regression strategies
-            if isinstance(X.iloc[0, 0], pd.Series):
-                # todo investigate incorporating the reshaping into the data extraction instead of this 2-stage process
-                X = np.array([[X.iloc[r, c].values for c in range(len(X.columns))] for r in range(len(X))])
-                X = X.reshape(X.shape[0], X.shape[2], X.shape[1])  # go from [n][d][m] to [n][m][d]
-            else:
-                X = X.to_numpy()
+        if X.shape[1] > 1 and _is_nested_dataframe(X):
+            # we have multiple columns, AND each cell contains a series, so this is a multidimensional problem
+            X = _multivariate_df_to_array(X)
         else:
-            X = np.asarray([a.values for a in X.iloc[:, 0]])
+            # we either have a single column containing a series, or multiple columns each containing a primitive
+            # in either case, treat this as a univariate problem
+            X = _univariate_df_to_array(X)
 
     if len(X.shape) == 2:
         # add a dimension to make it multivariate with one dimension
         X = X.reshape(X.shape[0], X.shape[1], 1)  # go from [n][m] to [n][m][d=1]
 
     return X
+
+
+def _is_nested_dataframe(X):
+    return isinstance(X.iloc[0, 0], pd.Series)
+
+def _univariate_df_to_array(X):
+    return tabularise(X, return_array=True)
+
+def _multivariate_df_to_array(X):
+    # tabularise at time of checking will not keep multivariate dimensions separate, e.g. [n][m][d] becomes [n][m*d]
+    # todo investigate incorporating the reshaping into the data extraction instead of this 2-stage process
+    X = np.array([[X.iloc[r, c].values for c in range(len(X.columns))] for r in range(len(X))])
+    return X.reshape(X.shape[0], X.shape[2], X.shape[1])  # go from [n][d][m] to [n][m][d]
