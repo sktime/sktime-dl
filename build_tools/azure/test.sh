b536a7deeb12af41ee59c670ec80a1404a039171
mloning <markus.loning.17@ucl.ac.uk>
2020-04-26 17:00:18 +0100

    more changes

---
+++ build_tools/azure/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# adapted from https://github.com/scikit-learn/scikit-learn/blob/master/build_tools/travis/test_script.sh
+
+# exit the script if any statement returns a non-true return value
+set -e
+
+# activate conda environment
+source activate testenv
+
+# print test environment
+conda list
+
+# Get into a temp directory to run test from the installed scikit-learn and
+# check if we do not leave artifacts
+mkdir -p "$TEST_DIR"
+
+# We need to copy the setup.cfg for the pytest settings
+cp setup.cfg "$TEST_DIR"
+cd "$TEST_DIR"
+
+set -x  # print executed commands to the terminal
+
+pytest -m="not slow" --verbose --showlocals --durations=20 --junitxml=junit/test-results.xml --cov=sktime_dl --pyargs ../sktime_dl
+
+set +e
