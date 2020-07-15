8f5ecc34d0d8348fc4b331c9c5d24000f3e6e8e6
Aaron Bostrom <aaron.bostrom1@gmail.com>
2020-07-15 12:06:13 +0100

    start of a fix fore forecasting regressor tests. but should be a different PR

--- sktime_dl/deeplearning/tests/test_regressors.py
+++ sktime_dl/deeplearning/tests/test_regressors.py
@@ -1,16 +1,16 @@
 import numpy as np
-# import pandas as pd
 # from sklearn.metrics import mean_squared_error
 from sktime.datasets import load_italy_power_demand
-# from sktime.datasets import load_shampoo_sales
-# from sktime.benchmarking.strategies import TSRStrategy
-# from sktime.benchmarking.tasks import TSRTask
-# from sklearn.pipeline import Pipeline
-# from sktime.transformers.series_as_features.reduce import Tabularizer
-
+# from sktime.datasets import load_airline
+# from sktime.forecasting.compose import ReducedRegressionForecaster
+# from sktime.forecasting.model_selection import temporal_train_test_split
+# from sktime.forecasting.compose import ReducedTimeSeriesRegressionForecaster
 from sktime_dl.deeplearning import MLPRegressor
-from sktime_dl.utils.model_lists import SMALL_NB_EPOCHS
-from sktime_dl.utils.model_lists import construct_all_regressors
+from sktime_dl.utils.model_lists import (SMALL_NB_EPOCHS,
+                                         construct_all_regressors)
+# from sktime.utils.data_container import tabularize
+
+#
 
 
 def test_regressor(estimator=MLPRegressor(nb_epochs=SMALL_NB_EPOCHS)):
@@ -38,7 +38,7 @@ def test_regressor(estimator=MLPRegressor(nb_epochs=SMALL_NB_EPOCHS)):
 
 
 # def test_regressor_forecasting(
-#         estimator=MLPRegressor(nb_epochs=SMALL_NB_EPOCHS), window_length=4
+#         regressor=MLPRegressor(nb_epochs=SMALL_NB_EPOCHS), window_length=4
 # ):
 #     """
 #     test a regressor used for forecasting
@@ -46,28 +46,25 @@ def test_regressor(estimator=MLPRegressor(nb_epochs=SMALL_NB_EPOCHS)):
 #     print("Start test_regressor_forecasting()")
 
 #     # get data into expected nested format
-#     shampoo = load_shampoo_sales(return_y_as_dataframe=True)
-#     train = pd.DataFrame(
-#         pd.Series([shampoo.iloc[0, 0].iloc[:24]]), columns=shampoo.columns
-#     )
-#     update = pd.DataFrame(
-#         pd.Series([shampoo.iloc[0, 0].iloc[:30]]), columns=shampoo.columns
-#     )
+#     y = load_airline()
+#     y_train, y_test = temporal_train_test_split(y, test_size=36)
+
+#     # y_train = tabularize(y_train, return_array=True)
+#     # y_test = tabularize(y_test, return_array=True)
+
+#     # from sklearn.neighbors import KNeighborsRegressor
+#     # regressor = KNeighborsRegressor(n_neighbors=1)
 
 #     # define simple time-series regressor using time-series as features
-#     steps = [("tabularise", Tabularizer()), ("rgs", estimator)]
-#     estimator = Pipeline(steps)
-
-#     task = TSRTask(target="ShampooSales", fh=[1], metadata=train)
-#     strategy = TSRStrategy(
-#         estimator=estimator, window_length=window_length
+#     forecaster = ReducedRegressionForecaster(
+#         regressor=regressor, window_length=window_length
 #     )
-#     strategy.fit(task, train)
-#     y_pred = strategy.predict()
+#     forecaster.fit(y_train)
+#     fh = np.arange(len(y_test)) + 1
+#     y_pred = forecaster.predict(fh)
 
 #     # Compare the prediction to the test data
-#     test = update.iloc[0, 0][y_pred.index]
-#     mse = np.sqrt(mean_squared_error(test, y_pred))
+#     mse = np.sqrt(mean_squared_error(y_test, y_pred))
 #     print("Error:", mse)
 #     print("End test_regressor_forecasting()")
 
@@ -78,21 +75,12 @@ def test_all_regressors():
         test_regressor(network)
         print("\t\t" + network.__class__.__name__ + " testing finished")
 
-
 # def test_all_forecasters():
 #     window_length = 8
 
 #     for network in construct_all_regressors(SMALL_NB_EPOCHS):
-#         print(
-#             "\n\t\t" + network.__class__.__name__ + " forecast
-#  testing started"
-#         )
+#         print("\n\t\t" + network.__class__.__name__ + " forecasttesting \
+#          started")
 #         test_regressor_forecasting(network, window_length=window_length)
-#         print(
-#             "\t\t" + network.__class__.__name__ + " forecast
-# testing finished"
-#         )
-
-
-# if __name__ == "__main__":
-#     test_all_regressors()
+#         print("\t\t" + network.__class__.__name__ + " forecasttesting \
+#             finished")
