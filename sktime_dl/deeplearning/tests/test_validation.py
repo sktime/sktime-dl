56a08b0031ba3708b06d3ee63395da24a739009d
James Large <james.large@uea.ac.uk>
2020-08-11 20:04:13 +0100

    missed test fix

--- sktime_dl/deeplearning/tests/test_validation.py
+++ sktime_dl/deeplearning/tests/test_validation.py
@@ -15,11 +15,11 @@ def test_validation(network=MLPClassifier()):
 
     X_train, y_train = load_italy_power_demand(split="train", return_X_y=True)
 
-    X_test = X_train[5:10]
-    y_test = y_train[5:10]
+    X_test = X_train[10:20]
+    y_test = y_train[10:20]
 
-    X_train = X_train[:5]
-    y_train = y_train[:5]
+    X_train = X_train[:10]
+    y_train = y_train[:10]
 
     if isinstance(network, BaseRegressor):
         # Create some regression values, taken from test_regressor
