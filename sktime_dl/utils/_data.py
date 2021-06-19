1846ecce94b32e656b35871512939fe23b82c7c2
Tony Bagnall <ajb@uea.ac.uk>
2021-06-19 17:47:26 +0100

    remove data_processing import in tests

--- sktime_dl/utils/_data.py
+++ sktime_dl/utils/_data.py
@@ -3,7 +3,7 @@
 __author__ = "James Large"
 
 import pandas as pd
-from sktime.utils.data_processing import from_nested_to_3d_numpy
+from sktime.utils.data_io import from_nested_to_3d_numpy
 from sktime.utils.validation.panel import check_X, check_X_y
 
 
