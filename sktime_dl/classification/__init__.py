833cf04e63c78dba35c3e95b7a1cdd25201dbc2d
Sandipan Goswami <sandi.goswami70@gmail.com>
2021-07-09 17:06:37 +0100

    Refactor_rest

--- sktime_dl/classification/__init__.py
+++ sktime_dl/classification/__init__.py
@@ -3,9 +3,23 @@ __all__ = [
     "FCNClassifier",
     "InceptionTimeClassifier",
     "LSTMFCNClassifier",
+    "CNTCClassifier",
+    "MCDCNNClassifier",
+    "MCNNClassifier",
+    "MLPClassifier",
+    "ResNetClassifier",
+    "TLENETClassifier",
+    "TWIESNClassifier",
 ]
 
 from sktime_dl.classification._cnn import CNNClassifier
 from sktime_dl.classification._fcn import FCNClassifier
 from sktime_dl.classification._inceptiontime import InceptionTimeClassifier
 from sktime_dl.classification._lstmfcn import LSTMFCNClassifier
+from sktime_dl.classification._cntc import CNTCClassifier
+from sktime_dl.classification._mcdcnn import MCDCNNClassifier
+from sktime_dl.classification._mcnn import MCNNClassifier
+from sktime_dl.classification._mlp import MLPClassifier
+from sktime_dl.classification._resnet import ResNetClassifier
+from sktime_dl.classification._tlenet import TLENETClassifier
+from sktime_dl.classification._twiesn import TWIESNClassifier
