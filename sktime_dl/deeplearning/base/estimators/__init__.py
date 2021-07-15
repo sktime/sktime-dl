d1668479a74971964cef3e439e5ec78f31b7aca8
Tony Bagnall <ajb@uea.ac.uk>
2021-07-15 14:25:29 +0100

    remove deep learning directory
    refactor tests to top level code

--- sktime_dl/deeplearning/base/estimators/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = [
-    "BaseDeepNetwork",
-    "BaseDeepRegressor",
-    "BaseDeepClassifier"
-]
-
-from sktime_dl.classification._classifier import BaseDeepClassifier
-from sktime_dl.networks._network import BaseDeepNetwork
-from sktime_dl.regression._regressor import BaseDeepRegressor
