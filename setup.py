25a7999f033b213394c39f15019cbfcb5ebefb28
Jack Russon <51986332+jnrusson1@users.noreply.github.com>
2021-08-03 22:17:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def find_install_requires():
     """
 
     install_requires = [
-        'sktime==0.7',
+        'sktime==0.7.0',
         'h5py>=2.8.0',
         'matplotlib',
         'seaborn',
